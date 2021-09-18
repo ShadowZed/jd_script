fba9b46d98e06ccf74551848744d8eed13ccd2cf
ShadowZed <32790988+ShadowZed@users.noreply.github.com>
2021-09-18 10:48:20 +0800

    东东农场互助码

--- jdFruitShareCodes.js
+++ jdFruitShareCodes.js
@@ -7,15 +7,15 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let FruitShareCodes = [
-  '79cebb1893b24f428566bc9f97c15caf@8582712d770c4f6d828abf34e92e5a75@6fbd26cc27ac44d6a7fed34092453f77@61ff5c624949454aa88561f2cd721bf6@56db8e7bc5874668ba7d5195230d067a',//账号一的好友shareCode,不同好友中间用@符号隔开
-  '6fbd26cc27ac44d6a7fed34092453f77@61ff5c624949454aa88561f2cd721bf6@9c52670d52ad4e1a812f894563c746ea@8175509d82504e96828afc8b1bbb9cb3',//账号二的好友shareCode，不同好友中间用@符号隔开
+  '79cebb189311b24f428566bc9f97c15caf@8582712d77011c4f6d828abf34e92e5a75',//账号一的好友shareCode,不同好友中间用@符号隔开
+  '6fbd26cc27ac44d6a7fed3409242253f77@61ff5c624949454aa88561f2cd721b22f6',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 
 // 从日志获取互助码
-// const logShareCodes = require('./utils/jdShareCodes');
-// if (logShareCodes.FRUITSHARECODES.length > 0 && !process.env.FRUITSHARECODES) {
-//   process.env.FRUITSHARECODES = logShareCodes.FRUITSHARECODES.join('&');
-// }
+const logShareCodes = require('./utils/jdShareCodes');
+if (logShareCodes.FRUITSHARECODES.length > 0 && !process.env.FRUITSHARECODES) {
+  process.env.FRUITSHARECODES = logShareCodes.FRUITSHARECODES.join('&');
+}
 
 // 判断github action里面是否有东东农场互助码
 if (process.env.FRUITSHARECODES) {
